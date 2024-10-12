ad15f02892d85e74a1b9ae0dafd76d181d5042dd
busayo524 <olowookerebusayo66@gmail.com>
2024-10-12 04:32:07 +0000

    annotate function for element length with duck typing

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Let's duck type an iterable object
+"""
+from typing import List, Sequence, Iterable, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Return list
+    """
+    return [(i, len(i)) for i in lst]
