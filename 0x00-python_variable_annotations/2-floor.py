42e21c7ba3fb968f5245aa2c2fdf7449c0e193df
busayo524 <olowookerebusayo66@gmail.com>
2024-10-12 04:17:43 +0000

    implement type-annotated function to return the floor of a float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""floor of arguement"""
+
+
+def floor(n: float) -> int:
+    """function returns the floor of n
+    Args:
+        n - float number
+    """
+    return int(n)
