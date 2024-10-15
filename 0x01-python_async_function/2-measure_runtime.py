260049b693a8ae1c058be05d6d6448aec8dc3c33
busayo524 <olowookerebusayo66@gmail.com>
2024-10-15 13:34:08 +0000

    add function to measure execution time of async tasks and return average runtime

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Measure the total runtime/execution time of the function
+"""
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    Measure the time it takes to run `wait_n` with `n` coroutines
+    that wait for random amounts of time up to `max_delay` seconds.
+    Returns the average time taken per coroutine.
+    """
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    return (time.time() - start_time) / n
